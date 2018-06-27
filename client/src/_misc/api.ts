99ee079410c72eeb517031e5cf41f7c01c437d70
radacx <kolotocar94@gmail.com>
2018-06-27 22:43:49 +0200

    Finalize backend settings

--- client/src/_misc/api.ts
+++ client/src/_misc/api.ts
@@ -9,9 +9,11 @@ const template = (strings: any, ...keys: any[]) =>
     return result.join('');
   });
 
-export const RESTAURANTS = '/restaurants';
+const BASE = 'http://localhost:4567';
+
+export const RESTAURANTS = `${BASE}/restaurants`;
 export const RESTAURANTS_FREE_TABLES = template`${RESTAURANTS}/${0}/free_tables`;
 
-export const RESERVATIONS = '/reservations';
+export const RESERVATIONS = `${BASE}/reservations`;
 export const RESERVATIONS_CREATE = template`${RESTAURANTS}/${0}/create_reservation`;
 export const RESERVATIONS_REMOVE = template`${RESERVATIONS}/${0}/remove`;
--- client/src/_misc/backend.ts
+++ client/src/_misc/backend.ts
@@ -12,7 +12,7 @@ import { INewReservation } from "../_types/INewReservation";
 import { IFreeTablesRequest } from "../_types/IFreeTablesRequest";
 import { IReservation } from "../_types/IReservation";
 
-export const realBackend = {
+export const backend = {
   getRestaurants: (): Promise<IRestaurant[]> =>
     HttpClient.get<IRestaurant[]>(RESTAURANTS),
   getReservations: (): Promise<IReservation[]> =>
@@ -24,50 +24,3 @@ export const realBackend = {
   createReservation: (restaurantId: number, reservation: INewReservation): Promise<void> =>
     HttpClient.post(RESERVATIONS_CREATE(restaurantId), reservation),
 };
-
-type Backend = typeof realBackend;
-
-const restaurants: IRestaurant[] = [
-  { id: 1, name: "res1" },
-  { id: 12, name: "res2" },
-  { id: 14, name: "res3" },
-  { id: 123, name: "RESTAURACIA " },
-];
-const tables: ITable[] = [
-  { id: 1, seats: 3, number: 34, freeTimeBlocks: [ 2, 4, 5 ] },
-  { id: 2, seats: 2, number: 36, freeTimeBlocks: [ 5, 6 ] },
-];
-let reservations: IReservation[] = [
-  { id: 3, block: 5, day: new Date(), restaurant: { id: 1, name: 'Trolo' }, forName: 'yourself' }
-];
-
-export const fakeBackend: Backend = {
-  getRestaurants: () => Promise.resolve(restaurants),
-  getReservations: () => Promise.resolve(reservations),
-  removeReservation: async (id: number) => {
-    reservations = reservations.filter(r => r.id !== id);
-  },
-  loadTables: () => Promise.resolve(tables),
-  createReservation: async (restaurantId: number, { block, day, tableId, forName }: INewReservation) => {
-    const newReservation: IReservation = {
-      id: Math.random(),
-      restaurant: restaurants.find(r => r.id === restaurantId)!,
-      block,
-      day,
-      forName,
-    };
-
-    reservations.push(newReservation);
-
-    const table = tables.find(t => t.id === tableId);
-    const newTimeBlocks = table!.freeTimeBlocks.filter(tB => tB !== block);
-
-    if (newTimeBlocks.length === 0) {
-      delete tables[tableId];
-    } else {
-      table!.freeTimeBlocks = newTimeBlocks;
-    }
-  },
-};
-
-export const backend = fakeBackend;
